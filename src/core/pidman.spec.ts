edea5a1b6a91cca38cba2156fa2d5d777c53475c
Nicolas Iglesias <nfiglesias@gmail.com>
2020-05-06 14:43:17 -0300

    finish Pidman spec

--- src/core/pidman.spec.ts
+++ src/core/pidman.spec.ts
@@ -1,16 +1,18 @@
-import { Pidman } from './pidman';
+import { Pidman } from './';
+import { PidmanGroup } from './group';
 import { PidmanStringUtils } from '../utils';
 import { mocked } from 'ts-jest/utils';
 
+jest.mock('./group');
 jest.mock('../utils');
 
 const mockedStringUtils = mocked(PidmanStringUtils, true);
+const mockedGroup = mocked(PidmanGroup, true);
 
 describe('initializing pidman', () => {
   beforeAll(() => mockedStringUtils.getId.mockReturnValue('test'));
 
   test('construct without options', () => {
-
     const pidman = new Pidman();
 
     expect(pidman).toBeInstanceOf(Pidman);
@@ -24,5 +26,39 @@ describe('initializing pidman', () => {
 
     expect(pidman).toBeInstanceOf(Pidman);
     expect(pidman.getOptions().id).toBe('newid');
+  });
+});
+
+describe('adding and running groups', () => {
+  let pidman: Pidman;
+
+  beforeAll(() => {
+    pidman = new Pidman();
+  });
+
+  test('adding a group in object form', () => {
+    pidman.addProcessGroup({
+      id: 'newgroup'
+    });
+
+    expect(PidmanGroup).toHaveBeenCalledTimes(1);
+    expect(pidman.getProcessGroups()).toHaveLength(1);
+  });
+
+  test('adding a group in instance form', () => {
+    const group = new PidmanGroup({});
+    pidman.addProcessGroup(group);
+
+    expect(pidman.getProcessGroups()).toHaveLength(2);
+  });
+
+  test('running all groups', () => {
+    const group1 = mockedGroup.mock.instances[0];
+    const group2 = mockedGroup.mock.instances[1];
+
+    pidman.run();
+
+    expect(group1.run).toHaveBeenCalledTimes(1);
+    expect(group2.run).toHaveBeenCalledTimes(1);
   });
 });
